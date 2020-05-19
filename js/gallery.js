d1b8d91e50603b49aae809960bc65e5afab8b3ec
Amardeep Sarang <amar.sarang98@gmail.com>
2020-05-19 13:35:12 -0400

    added controls to see different edits

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,7 @@
-
-$(".img_box").click(function(){
+//////////////////////////////////////////////////////
+// Script for show/hide tool bar
+///////////////////////////////////////////////////////
+$(".img_box").click(function () {
 
     //remove from all other pics
     $(".img_box .pic-control-bar").removeClass("pic-control-bar-show")
@@ -8,8 +10,101 @@ $(".img_box").click(function(){
 })
 
 
-$(".edit-sl-bn").click(function(){
-    console.log($(this).parent().parent().parent().children(".pic-edit-picker").html())
+$(".edit-sl-bn").click(function () {
     //toggle the edit selector bar
     $(this).parent().parent().parent().children(".pic-edit-picker").toggleClass("pic-edit-picker-show")
 })
+
+//////////////////////////////////////////////////////
+// Script for slecting edit by hovering/click
+///////////////////////////////////////////////////////
+
+//image
+mainSrc = "";
+$(".pic-edit-picker span img").hover(
+    function () {
+        //hover in
+        var mainImg = $(this).parent().parent().parent().children('img')
+        mainSrc = mainImg.attr('src')//store the src of the main image
+
+
+        mainImg.attr('src', $(this).attr('src'));//swap to image
+    }, function () {
+        //hover out, change back to origina;
+        var mainImg = $(this).parent().parent().parent().children('img')
+        var editBar =$(this).parent().parent().parent().children('.pic-edit-picker')
+        mainImg.attr('src', mainSrc)
+        setActive(editBar, mainSrc)
+    }
+);
+
+$(".pic-edit-picker span img").click(
+    function () {
+        //permanently change the main image to the new edit
+        var mainImg = $(this).parent().parent().parent().children('img')
+        mainSrc = mainImg.attr('src')
+
+    }
+);
+
+//////////////////////////////////////////////////////
+// Script for slecting edit using arrows
+///////////////////////////////////////////////////////
+function getSrcList(editBar) {
+    var srcList = new Array();
+    $(editBar).children('span').each(function () {
+
+        var imgsrc = $(this).children('img').attr('src');
+        //console.log(imgsrc);
+        srcList.push(imgsrc);
+    });
+
+    return srcList;
+}
+function arrowClickHandle(target,direction) {
+    //get main image and edit bar objects
+    var mainImg = $(target).parent().parent().parent().children('img')
+    var src = mainImg.attr('src')
+    var editBar = $(target).parent().parent().parent().children('.pic-edit-picker')
+    //get the src path for all edits in the bar
+    var srcList = getSrcList(editBar);
+    //find current place in list
+    var i = srcList.indexOf(src)
+    
+    //get next index with wrap
+    if (direction == 'r') {
+        //right arrow click
+        i = (i + 1) % srcList.length
+    } else {
+        //left arrow click
+        i=i-1
+        if(i<0){
+            i=srcList.length-1;
+        }
+        
+    }
+    //assign new image to main image
+    mainImg.attr('src', srcList[i])
+    setActive(editBar, srcList[i])
+}
+$(".edit-sl-arw-r").click(function(){arrowClickHandle(this,'r')}
+);
+
+$(".edit-sl-arw-l").click(function(){arrowClickHandle(this,'l')}
+);
+
+function setActive(editBar, mainImgSrc){
+    //set the image to have selected style if it is the one selected
+    $(editBar).children('span').each(function () {
+
+        var img = $(this).children('img');
+        
+        if(img.attr('src')==mainImgSrc){
+            //added selected class
+            img.addClass('pic-edit-picked')
+        }else{
+            //remove selected class
+            img.removeClass('pic-edit-picked')
+        }
+    });
+}
