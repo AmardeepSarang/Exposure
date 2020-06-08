f2eef04c717d6d243caf2d151861c812a83709e2
Amardeep Sarang <amar.sarang98@gmail.com>
2020-06-08 13:46:09 -0400

    Started to add code to handle edit upload

---
+++ post.php
@@ -0,0 +1,161 @@
+<?php
+include_once 'include/config.php';
+//check if this is an original post or an edited image post
+$edit_of = -1;
+$db = "";
+if (isset($_GET['edit_of'])) {
+    $edit_of = $_GET['edit_of'];
+    // Connect to MySQL
+    $db = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+
+    // Error checking
+    if (!$db) {
+        print "<p>Error - Could not connect to MySQL</p>";
+        exit;
+    }
+    $error = mysqli_connect_error();
+
+    if ($error != null) {
+        $output = "<p>Unable to connet to database</p>" . $error;
+        exit($output);
+    }
+}
+
+function addTitle($db, $edit_of)
+{
+    //get title of original to fill input (if posting edit of image)
+    if ($edit_of != -1) {
+        $query = "SELECT * FROM `image` WHERE `Img_id`=$edit_of";
+
+        $result = mysqli_query($db, $query);
+        $row = mysqli_fetch_assoc($result);
+        echo "value = '".$row['title']."'";
+    } else {
+        echo "";
+    }
+}
+function addTags($db, $edit_of)
+{
+    //get tags of original to fill input (if posting edit of image)
+    if ($edit_of != -1) {
+        $query = "SELECT tag.tag_name FROM img_tag, tag WHERE img_tag.tag_id=tag.tag_id AND img_tag.img_id=$edit_of";
+
+        $result = mysqli_query($db, $query);
+        
+        $tag_str="";
+        while ($row = mysqli_fetch_assoc($result)) {
+            //make a string of tags
+            $tag_str =$tag_str. $row["tag_name"]." ";
+        }
+        echo "value = '".$tag_str."'";
+    } else {
+        echo "";
+    }
+}
+function addDes($db, $edit_of)
+{
+    //get description of original to fill input (if posting edit of image)
+    if ($edit_of != -1) {
+        $query = "SELECT * FROM `image` WHERE `Img_id`=$edit_of";
+
+        $result = mysqli_query($db, $query);
+        $row = mysqli_fetch_assoc($result);
+        echo "value = '".$row['description']."'";
+    } else {
+        echo "";
+    }
+}
+
+function addBtnTitle($edit_of)
+{
+    //prints btn text based on if post is origanl or edited img
+    if ($edit_of != -1) {
+        echo "Post Edited Image";
+    } else {
+        echo "Post Image";
+    }
+}
+
+function addParam($db,$edit_of)
+{
+
+
+    //adds extra param to form based on if post is origanl or edited img
+    if ($edit_of != -1) {
+        $query = "SELECT * FROM `image` WHERE `Img_id`=$edit_of";
+
+        $result = mysqli_query($db, $query);
+        $row = mysqli_fetch_assoc($result);
+        echo '<input type="hidden" name="edit_of" value="'.$edit_of.'" />';
+        echo '<input type="hidden" name="upload_by" value="'.$row['uploaded_by'].'" />';
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lan="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Image</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/57c4c79ee8.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+    <!--
+        main nav bar
+    -->
+    <nav class="main-nav">
+        <div class="logo">
+            <img src="images/logo.png">
+
+        </div>
+        <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <div class="post-bt">
+            <a href="#"><i class="fas fa-plus"></i>&nbsp Post new image</a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="#"><i class="fas fa-th"></i>&nbsp Gallery</a></li>
+            <li><a href="#"><i class="fas fa-house-user"></i>&nbsp My dashboard</a></li>
+            <li><a href="#"><i class="fas fa-sign-out-alt"></i>&nbsp Sign out</a></li>
+            <li><a href="#"><i class="fas fa-question-circle"></i>&nbsp About</a></li>
+        </ul>
+    </nav>
+
+    <div class="form-box">
+        <form action="upload.php" method="POST" enctype="multipart/form-data">
+            <div class="image-preview">
+                <img src="images/placeholder_preview.png" alt="">
+
+            </div>
+            <div id="titleInput">Title:<input type="text" <?php addTitle($db, $edit_of) ?> name="title" placeholder="Enter title"></div>
+
+            <div>
+                Tags (seperate tages by spaces):<br> <input type="text" <?php addTags($db, $edit_of) ?>  name="tags" placeholder="Enter tags (optional)">
+            </div>
+            <div>
+                Description:<input type="text" name="description" <?php addDes($db, $edit_of) ?> placeholder="Enter description (optional)">
+            </div>
+            <div id="filediv">
+                File: <input id="imginput" type="file" name="file">
+            </div>
+            <?php addParam($db,$edit_of)?>
+            <button type="submit" name="submit"><?php addBtnTitle($edit_of)?></button>
+        </form>
+    </div>
+    <script src="js/post.js"></script>
+</body>
--- upload.php
+++ upload.php
@@ -57,7 +57,7 @@ function getIdByPath($db, $path)
     //get image id using path name
     $query = "SELECT * FROM `image` WHERE Img_file_name = '$path'";
     $result = mysqli_query($db, $query);
-    
+
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         $row = mysqli_fetch_assoc($result);
@@ -71,7 +71,7 @@ function getTagIdByname($db, $name)
     //get tag id if the tag exits
     $query = "SELECT * FROM `tag` WHERE tag_name = '$name'";
     $result = mysqli_query($db, $query);
-    
+
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         $row = mysqli_fetch_assoc($result);
@@ -81,7 +81,8 @@ function getTagIdByname($db, $name)
     return false;
 }
 
-function insertTag($db,$tag){
+function insertTag($db, $tag)
+{
     //inserts new tag and returns id
     //use sanitation
     $query = "INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES (NULL, ?);";
@@ -96,7 +97,7 @@ function insertTag($db,$tag){
         $result = mysqli_stmt_execute($statement);
 
         if ($result) {
-            return getTagIdByname($db,$tag);
+            return getTagIdByname($db, $tag);
         } else {
             print "Mysql insert Error" . mysqli_stmt_error($statement);
         }
@@ -104,14 +105,36 @@ function insertTag($db,$tag){
 }
 
 
-function insertImgTag($db,$imgId,$tagId){
+function insertImgTag($db, $imgId, $tagId)
+{
     //inserts img tag relation in img_tag table
     //use sanitation
     $query = "INSERT INTO `img_tag` (`img_id`, `tag_id`) VALUES (?, ?);";
     if ($statement = mysqli_prepare($db, $query)) {
 
         // bind parameters s - string,
-        $result = mysqli_stmt_bind_param($statement, 'ss', $imgId,$tagId);
+        $result = mysqli_stmt_bind_param($statement, 'ss', $imgId, $tagId);
+        if (!$result) {
+            print "<p>bounding error</p>";
+        }
+        // execute query
+        $result = mysqli_stmt_execute($statement);
+
+        if (!$result) {
+            print "Mysql insert Error" . mysqli_stmt_error($statement);
+        }
+    }
+}
+
+function insertImgedit($db, $imgId, $orgId)
+{
+    //inserts img edit relation in img_edit table
+    //use sanitation
+    $query = "INSERT INTO `img_edit` (`img_id`, `edit_id`) VALUES (?, ?);";
+    if ($statement = mysqli_prepare($db, $query)) {
+
+        // bind parameters s - string,
+        $result = mysqli_stmt_bind_param($statement, 'ss', $imgId, $orgId);
         if (!$result) {
             print "<p>bounding error</p>";
         }
@@ -125,93 +148,97 @@ function insertImgTag($db,$imgId,$tagId){
 }
 ?>
 <div class="form-box">
-<?php
-if (isset($_POST['submit'])) {
-    $file = $_FILES['file'];
-    $path = uploadFile($file);
-    $title = $_POST['title'];
-    $desc = $_POST['description'];
-    $tags = $_POST['tags'];
-    //seperate tags
-    $tagList = explode(' ', $tags);
-    /*
-    echo "<br>";
-    echo $path;
-    echo "<br>";
-    echo $title;
-    echo "<br>";
-    echo $desc;
-    echo "<br>";
-    echo $tags;
-    echo "<br>";
-    print_r($tagList);
-*/
+    <?php
+    if (isset($_POST['submit'])) {
+        $file = $_FILES['file'];
+        $path = uploadFile($file);
+        $title = $_POST['title'];
+        $desc = $_POST['description'];
+        $tags = $_POST['tags'];
+        //seperate tags
+        $tagList = explode(' ', $tags);
 
 
-    /////////////////////
+        // Connect to MySQL
+        $db = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 
-    // Connect to MySQL
-    $db = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
-
-    // Error checking
-    if (!$db) {
-        print "<p>Error - Could not connect to MySQL</p>";
-        exit;
-    }
-    $error = mysqli_connect_error();
-
-    if ($error != null) {
-        $output = "<p>Unable to connet to database</p>" . $error;
-        exit($output);
-    }
-
-    //insert imag
-    $user = 1;
-    $is_original = 1;
-    //use sanitation
-    $query = "INSERT INTO `image` (`Img_id`, `title`, `Img_file_name`, `edited_by`, `uploaded_by`, `is_original`, `uploaded_on`, `description`) VALUES (NULL, ?, ?, NULL, ?, ?, current_timestamp(), ?)";
-    if ($statement = mysqli_prepare($db, $query)) {
-
-        // bind parameters s - string,
-        $result = mysqli_stmt_bind_param($statement, 'sssss', $title, $path, $user, $is_original, $desc);
-        if (!$result) {
-            print "<p>bounding error</p>";
+        // Error checking
+        if (!$db) {
+            print "<p>Error - Could not connect to MySQL</p>";
+            exit;
         }
-        // execute query
-        $result = mysqli_stmt_execute($statement);
+        $error = mysqli_connect_error();
 
-        if ($result) {
-            print "<p>Your image post was successful</p>";
-        } else {
-            print "Mysql insert Error" . mysqli_stmt_error($statement);
+        if ($error != null) {
+            $output = "<p>Unable to connet to database</p>" . $error;
+            exit($output);
         }
-    }
 
-    $imgId = getIdByPath($db, $path);
-    //echo $imgId;
+        //insert imag
+        $user = 111;
 
-    //for each tag see if it exits in tag table and the link it with img
-    foreach ($tagList as $tag){
-        $tag=strtolower($tag);
-        $id=getTagIdByname($db, $tag);
-        
-        if($id==false){
-            //no such tag so insert it
-    
-            $id=insertTag($db,$tag);
-           // echo $id ."(inserted) ".$tag."<br>";
-        }/*else{
+
+        $edit_of = -1;
+        $is_original = 1;
+        $upload_user = $user;
+        $edit_user = "NULL";
+        if (isset($_POST['edit_of'])) {
+            $edit_of = $_POST['edit_of'];
+            if (isset($_POST['upload_by'])) {
+                $upload_user = $_POST['upload_by'];
+                $edit_user = $user;
+            }
+
+            $is_original = 0;
+        }
+
+        //use sanitation
+        $query = "INSERT INTO `image` (`Img_id`, `title`, `Img_file_name`, `edited_by`, `uploaded_by`, `is_original`, `uploaded_on`, `description`) VALUES (NULL, ?, ?, ?, ?, ?, current_timestamp(), ?)";
+        if ($statement = mysqli_prepare($db, $query)) {
+
+            // bind parameters s - string,
+            $result = mysqli_stmt_bind_param($statement, 'ssssss', $title, $path,$edit_user, $upload_user, $is_original, $desc);
+            if (!$result) {
+                print "<p>bounding error</p>";
+            }
+            // execute query
+            $result = mysqli_stmt_execute($statement);
+
+            if ($result) {
+                print "<p>Your image post was successful</p>";
+            } else {
+                print "Mysql insert Error" . mysqli_stmt_error($statement);
+            }
+        }
+
+        $imgId = getIdByPath($db, $path);
+        //echo $imgId;
+
+        //for each tag see if it exits in tag table and the link it with img
+        foreach ($tagList as $tag) {
+            $tag = strtolower($tag);
+            $id = getTagIdByname($db, $tag);
+
+            if ($id == false) {
+                //no such tag so insert it
+
+                $id = insertTag($db, $tag);
+                // echo $id ."(inserted) ".$tag."<br>";
+            }/*else{
             echo $id ." ".$tag."<br>";
         }*/
 
-        //insert relation
-        insertImgTag($db,$imgId,$id);
+            //insert relation
+            insertImgTag($db, $imgId, $id);
+        }
+        if (isset($_POST['edit_of'])) {
+            //set new image to be an edit of old image
+            insertImgedit($db, $imgId, $edit_of);
+        }
+        mysqli_close($db);
     }
 
-    mysqli_close($db);
-}
 
-
-?>
-<button id="upload-con" onclick="window.location.href='gallery.html';">Continue</button>
+    ?>
+    <button id="upload-con" onclick="window.location.href='gallery.php';">Continue</button>
 </div>
