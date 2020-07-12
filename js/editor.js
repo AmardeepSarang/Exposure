c4381db6d51ebb4e52650d124ead3262b12a6fa4
Amardeep Sarang <amar.sarang98@gmail.com>
2020-07-12 13:23:13 -0400

    added canvas and upload popup to editor

---
+++ js/editor.js
@@ -0,0 +1,10 @@
+//controls for upload pop-up
+$("#edit-up").click(function () {
+    $('.upload-popup').toggleClass('upload-show')
+    
+});
+
+$("#up-can").click(function () {
+    $('.upload-popup').removeClass('upload-show')
+    
+});
